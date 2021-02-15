5894301026c858e43d7d181cad2ddffb5b455cee
knuspersnack <geo.d@posteo.de>
2021-02-15 22:48:05 +0100

    First RestController

---
+++ src/main/java/com/knuspersnack/rest/webservices/todoapp/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.knuspersnack.rest.webservices.todoapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping(path="/helloWorld")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping(path="/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+}
