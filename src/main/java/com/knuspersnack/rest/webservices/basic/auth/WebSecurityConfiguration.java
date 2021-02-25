a1ce71b67614fa21bfe7508e8be659da4e1f2858
knuspersnack <thumos@posteo.gr>
2021-02-25 21:54:55 +0100

    Auth and Cors config

---
+++ src/main/java/com/knuspersnack/rest/webservices/basic/auth/WebSecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.knuspersnack.rest.webservices.basic.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter
+{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS,"/**").permitAll() //Allow all option request
+                .anyRequest().authenticated().and().httpBasic(); //Except of option allow all basic auth requests
+
+    }
+}
