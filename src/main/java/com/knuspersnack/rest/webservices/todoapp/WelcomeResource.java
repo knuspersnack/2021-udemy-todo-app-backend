129aeef737b49c6c23722194e33fd7dcaa2c40f9
knuspersnack <thumos@posteo.gr>
2021-02-27 16:16:57 +0100

    Responde with a text

--- src/main/java/com/knuspersnack/rest/webservices/todoapp/WelcomeResource.java
+++ src/main/java/com/knuspersnack/rest/webservices/todoapp/WelcomeResource.java
@@ -1,18 +1,15 @@
 package com.knuspersnack.rest.webservices.todoapp;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class WelcomeResource {
 
     @GetMapping("/welcome/message/{username}")
-    public ResponseEntity<String> getAllTodos(@PathVariable String username) {
-        return ResponseEntity.ok("Welcome to the Todo-App. Here you can create and manage your ToDos quick!");
+    @ResponseBody
+    public String getAllTodos(@PathVariable String username) {
+        return "Welcome to the Todo-App. Here you can create and manage your ToDos quick!";
     }
 
 }
