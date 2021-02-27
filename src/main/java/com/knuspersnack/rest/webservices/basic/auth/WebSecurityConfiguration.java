5481144b220d69b384f669bb910e8f49489e6312
knuspersnack <thumos@posteo.gr>
2021-02-27 22:28:15 +0100

    Trying to solve CORS problems

--- src/main/java/com/knuspersnack/rest/webservices/basic/auth/WebSecurityConfiguration.java
+++ src/main/java/com/knuspersnack/rest/webservices/basic/auth/WebSecurityConfiguration.java
@@ -1,22 +1,44 @@
 package com.knuspersnack.rest.webservices.basic.auth;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter
-{
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS,"/**").permitAll() //Allow all option request
-                .anyRequest().authenticated().and().httpBasic(); //Except of option allow all basic auth requests
+                .antMatchers(HttpMethod.OPTIONS, "**").permitAll()//allow CORS option calls
+                .anyRequest().authenticated()
+                .and().formLogin().and().httpBasic();
 
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.cors();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200/*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","OPTIONS"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- src/main/java/com/knuspersnack/rest/webservices/todoapp/WelcomeResource.java
+++ src/main/java/com/knuspersnack/rest/webservices/todoapp/WelcomeResource.java
@@ -1,9 +1,11 @@
 package com.knuspersnack.rest.webservices.todoapp;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins={ "http://localhost:3000", "http://localhost:4200" })
 public class WelcomeResource {
 
     @GetMapping("/welcome/message/{username}")
